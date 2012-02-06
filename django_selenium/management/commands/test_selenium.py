cf468dd409061d61fd561668e05e840a0671a348
Chirag Jadwani <chirag.jadwani@gmail.com>
2012-02-06 22:37:30 +0530

    Fix database bug

--- django_selenium/management/commands/test_selenium.py
+++ django_selenium/management/commands/test_selenium.py
@@ -18,6 +18,33 @@ class Command(test.Command):
             help='Run only selenium tests (implies --selenium)')
     )
 
+    def fix_test_db_setting(self):
+        # If sqlite3 engine is used we need to update the test db setting
+        # because sqlite3 ':memory:' db does not allow multiple connections.
+        # Django defualts to use the fast ':memory:' db for tests but it
+        # can not be used as the selenium test server is run in a seperate
+        # thread which create a new connection to the db.
+        # Django 1.4 will fix this by adding support for sharing a single
+        # connection across threads.
+
+        from django.conf import settings
+        from django.core.exceptions import ImproperlyConfigured
+        import os.path
+
+        for db_name, db_settings in settings.DATABASES.items():
+            if db_settings['ENGINE'].split('.')[-1] == 'sqlite3':
+                test_name = db_settings.get('TEST_NAME')
+                if test_name is None:
+                    dirname, filename = os.path.split(db_settings['NAME'])
+                    filename = 'test_' + filename
+                    test_name = os.path.join(dirname, filename)
+                    db_settings['TEST_NAME'] = test_name
+                elif test_name == ':memory:':
+                    msg = 'settings[%r][%r] = %r\n' \
+                            '  Can not use ":memory:" when running selenium '\
+                            'tests' % (db_name, 'TEST_NAME', test_name)
+                    raise ImproperlyConfigured(msg)
+
     def handle(self, *test_labels, **options):
 
         verbosity = int(options.get('verbosity', 1))
@@ -27,6 +54,9 @@ class Command(test.Command):
         selenium_only = options.get('selenium_only', False)
         if selenium_only:
             selenium = True
+
+        if selenium:
+            self.fix_test_db_setting()
 
         TestRunner = get_runner(settings)
         test_runner = TestRunner(verbosity=verbosity, interactive=interactive, failfast=failfast,
